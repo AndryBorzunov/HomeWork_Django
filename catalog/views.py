8948e4fc41d4803d3d1f9e5bca4a40a29b85f634
Борзунов Андрей <andry73@yandex.ru>
2025-12-09 16:34:16 +0500

    README

--- catalog/views.py
+++ catalog/views.py
@@ -7,13 +7,13 @@ from catalog.models import Product
 def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'products_list.html', context)
+    return render(request, "products_list.html", context)
 
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
-    return render(request, 'product_detail.html', context)
+    return render(request, "product_detail.html", context)
 
 
 def contacts(request):
