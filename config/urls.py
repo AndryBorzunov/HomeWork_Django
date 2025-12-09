46d32a306b25f47af531bf42042680694933aa1a
Борзунов Андрей <andry73@yandex.ru>
2025-12-09 15:25:37 +0500

    product_detail

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_BOOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
